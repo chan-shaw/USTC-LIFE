98ce3a98e19212c679fd2efbc1839273938a9652
chanshaw <825403713@qq.com>
2019-03-28 18:30:19 +0800

    算法实验

---
+++ Algorithms/77.组合.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=77 lang=java
+ *
+ * [77] 组合
+ *
+ * https://leetcode-cn.com/problems/combinations/description/
+ *
+ * algorithms
+ * Medium (66.39%)
+ * Total Accepted:    8.3K
+ * Total Submissions: 12.6K
+ * Testcase Example:  '4\n2'
+ *
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * 
+ * 示例:
+ * 
+ * 输入: n = 4, k = 2
+ * 输出:
+ * [
+ * ⁠ [2,4],
+ * ⁠ [3,4],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ [1,3],
+ * ⁠ [1,4],
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> nums = new ArrayList<Integer>();
+
+        dfs(result, nums, n, k, 1);
+        return result;
+    }
+    public static void dfs(List<List<Integer>> result, List<Integer> nums, int n, int k, int start){
+        if(k==0) {
+			result.add(new ArrayList<Integer>(nums));
+			return;
+        }
+        
+        for(int i = start; i < n + 1; i++){
+            if(i > n - k + 1) break;
+            nums.add(i);
+            dfs(result, nums, n, k-1, i+1);
+            nums.remove(nums.size()-1);
+        }
+    }
+}
+
---
+++ Algorithms/771.jewels-and-stones.java
@@ -0,0 +1,76 @@
+
+/*
+ * @lc app=leetcode.cn id=771 lang=java
+ *
+ * [771] Jewels and Stones
+ *
+ * https://leetcode-cn.com/problems/jewels-and-stones/description/
+ *
+ * algorithms
+ * Easy (78.94%)
+ * Total Accepted:    29.4K
+ * Total Submissions: 37.2K
+ * Testcase Example:  '"aA"\n"aAAbbbb"'
+ *
+ *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ * 
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ * 
+ * 示例 1:
+ * 
+ * 输入: J = "aA", S = "aAAbbbb"
+ * 输出: 3
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: J = "z", S = "ZZ"
+ * 输出: 0
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * S 和 J 最多含有50个字母。
+ * J 中的字符不重复。
+ * 
+ * 
+ */
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        
+        if(J.isEmpty() || S.isEmpty()) return 0;
+
+        int count = 0;  // S 中 J 的字符的个数
+        char [] A = J.toCharArray(); 
+        char [] B = S.toCharArray(); 
+
+        boolean[] equalmap = new boolean[128];
+        
+        for(int i = 0; i < A.length;i++){
+            equalmap[A[i]] = true;
+        }
+
+        for(int i = 0; i < B.length; i++){
+            if(equalmap[B[i]])
+                count++;
+        }
+        return count;
+    }
+}
+
+/*
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        
+        int num = 0;
+        for(int i=0;i<S.length();i++){
+            if(J.contains(S.charAt(i)+"")){
+                num++;
+            }
+        }
+    
+        return num;
+    }
+}
+*/
